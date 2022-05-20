f78f237cb6026bb6a41a4b2064a8578bd452c7a4
anurag-asr <anurjwt@gmail.com>
2022-05-21 00:09:57 +0530

    first commit

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Todoinput from './Todoinput'
+import Todolist from './Todolist'
+
+const TodoApp = () => {
+  return (
+    <div>
+        TodoApp
+        <Todoinput/>
+        <Todolist/>
+    </div>
+  )
+}
+
+export default TodoApp
---
+++ src/components/Todoinput.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Todoinput = () => {
+  return (
+    <div>
+      <input type="text" placeholder='new todo item'/>
+      <button onClick={()=>{}}>add</button>
+    </div>
+  );
+};
+
+export default Todoinput
